a1493c2736b97947bac33829f75320333f980329
Utkp5 <utkp200@gmail.com>
2022-12-12 19:06:33 +0530

    search filter

--- src/Data.js
+++ src/Data.js
@@ -4,73 +4,85 @@ const data = {
             id : 0,
             img : "https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGJvb2t8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
             title : "card 1",
-            describe : "desc 1",
+            describe : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae?",
+            showMore : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quaeLorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae"
         },
         {
             id : 1,
             img : "https://images.unsplash.com/photo-1589998059171-988d887df646?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGJvb2t8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
             title : "card 2",
-            describe : "desc 2",
+            describe : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae?",
+            showMore : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quaeLorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae"
         },
         {
             id : 2,
             img : "https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGJvb2t8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
             title : "card 3",
-            describe : "desc 3",
+            describe : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae?",
+            showMore : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quaeLorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae"
         },
         {
             id : 3,
             img : "https://images.unsplash.com/photo-1512820790803-83ca734da794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGJvb2t8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
             title : "card 4",
-            describe : "desc 4",
+            describe : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae?" ,
+            showMore : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quaeLorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae"
         },
         {
             id : 4,
             img : "https://media.istockphoto.com/id/1361312027/photo/little-african-american-girl-reading-book-on-bed.jpg?b=1&s=170667a&w=0&k=20&c=_22DObgpV4hHgFt53GVrQfUx4sNYgi2zIr7ZoTubr9M=",
             title : "card 1",
-            describe : "desc 1",
+            describe : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae?" ,
+            showMore : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quaeLorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae"
         },
         {
             id : 5,
             img : "https://media.istockphoto.com/id/1440617913/photo/bunch-of-books-and-schoolboy.jpg?b=1&s=170667a&w=0&k=20&c=HgHq0wsEUtxuoRJ36viUagHzgiRtrMzyBa8PMez73u4=",
             title : "card 2",
-            describe : "desc 2",
+            describe : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae?" ,
+            showMore : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quaeLorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae"
         },
         {
             id : 7,
             img : "https://media.istockphoto.com/id/1341381732/photo/young-student-soldier-in-uniform.jpg?b=1&s=170667a&w=0&k=20&c=PXjZ58u8CFaVczUPbTLIOooNCuf4ch839auGrukDyLc=",
             title : "card 3",
-            describe : "desc 3",
+            describe : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae?" ,
+            showMore : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quaeLorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae"
         },
         {
             id : 8,
             img : "https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGJvb2t8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
             title : "card 2",
-            describe : "desc 2",
+            describe : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae?" ,
+            showMore : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quaeLorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae"
         },
         {
             id : 9,
             img : "https://images.unsplash.com/photo-1589998059171-988d887df646?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGJvb2t8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
             title : "card 1",
-            describe : "desc 1",
+            describe : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae?" ,
+            showMore : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quaeLorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae"
         },
         {
             id : 10,
             img : "https://media.istockphoto.com/id/1351601106/photo/tourist-reading-a-book-sitting-in-front-of-the-river.jpg?b=1&s=170667a&w=0&k=20&c=KYPbA7HLmprejju9CaPWx37KlK9xzTgsCipXqVpVjBk=",
             title : "card 4",
-            describe : "desc 4",
+            describe : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae?" ,
+            showMore : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quaeLorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae"
         },
         {
             id : 11,
             img : "https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGJvb2t8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
             title : "card 2",
-            describe : "desc 2",
+            describe : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae?",
+            showMore : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quaeLorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae"
         },
         {
             id : 12,
             img : "https://images.unsplash.com/photo-1589998059171-988d887df646?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGJvb2t8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
             title : "card 1",
-            describe : "desc 1",
+            describe : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae?",
+            showMore : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quaeLorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quae"
         },
     ]
 }
--- src/Readmore/Readmore.js
+++ src/Readmore/Readmore.js
@@ -1,29 +1,30 @@
 import React from "react";
 import data from "../Data";
-import "./Readmore.css"
-import useCollapse from 'react-collapsed'
+import "./Readmore.css";
+import useCollapse from "react-collapsed";
+import { useState } from "react";
 
 function Readmore() {
-
-  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse()
+  
+  const [isExpanded, setExpanded] = useState(false)
+  const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded })
 
   return (
     <div className="first_div">
-    {
-      data.cardData.map((item) => {
+      {data.cardData.map((item) => {
         return (
           <div className="card">
-          <div>
-          <img src={item.img} alt="" className="card_img" />
-              </div>
-              <div className="card_p">
+            <div><img src={item.img} alt="" className="card_img" /></div>
+            <div className="card_p">
               <p>{item.title}</p>
-                <p>{item.describe}</p>
-              </div>
-              </div>
-        )
-      })
-    }
+              <p>{item.describe}</p>
+              <section {...getCollapseProps()}>Collapsed content 🙈</section>
+              <button {...getToggleProps({ onClick: () => 
+                setExpanded((prevExpanded) => !prevExpanded),})}>{isExpanded ? 'Collapse' : 'Expand'}</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
